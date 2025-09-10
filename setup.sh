d66a5fd9dda23fa668b34758d654d79835013ecc
Aayush Ranjan <111623667+aayushxrj@users.noreply.github.com>
2025-09-10 23:58:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,21 +18,15 @@ fi
 mkdir -p cmd/api
 mkdir -p internal/api/{handlers,router,middlewares}
 mkdir -p internal/models
-mkdir -p internal/repository/mongodb
 mkdir -p internal/repository/sqlconnect
 mkdir -p pkg/utils
-mkdir -p proto
 
 # Create essential files
+touch .env
 touch cmd/api/server.go
-touch cmd/api/.env
-touch internal/models/user.go
-touch internal/models/product.go
-touch internal/repository/mongodb/mongoconnect.go
+touch internal/api/router/router.go
 touch internal/repository/sqlconnect/sqlconfig.go
 touch pkg/utils/error_handling.go
-touch pkg/utils/jwt_processing.go
-touch proto/main.proto
 
 # Initialize Go module using the current folder name
 go mod init github.com/aayushxrj/$PROJECT_NAME
